ac24f76a26ac7c2e86afaa68454d94ffebd0e175
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-17 23:12:16 -0300

    If in js

---
+++ JS/if statement/if.js
@@ -0,0 +1,25 @@
+var age = 80;
+
+if(age >= 65){
+  console.log("You are a senior!");
+}
+else if(age >= 18){
+  console.log("You are an adult!");
+}
+else if(age < 0){
+  console.log("You haven't been born yet!");
+}
+else{
+  console.log("You are a child!");
+}
+
+/*
+var online = true;
+ 
+if(online){
+  console.log("You're online");
+}
+else{
+  console.log("You're offline");
+}
+*/
