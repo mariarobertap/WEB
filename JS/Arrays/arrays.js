f1cc387632b68db781b07be0c3c855ef2617af54
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-17 23:09:41 -0300

    Arrays in js

---
+++ JS/Arrays/arrays.js
@@ -0,0 +1,16 @@
+var cars = ["Mustang", "Corvette", "Jaguar"];
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+cars.push("Tesla") //Add a new element to the array
+cars.pop(); //remove the last element
+
+var lastCar = cars[cars.length-1];
+
+cads = cars.sort() //sorts an array alphabetically
+cars = cars.reverse(); //reverse sort
+
+console.log(cars);
+console.log(lastCar);
