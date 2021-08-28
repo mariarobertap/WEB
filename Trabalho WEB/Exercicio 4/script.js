fd3db20f8c60b793bf9a6ed598fe20b3e8cc1f4f
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-28 01:06:02 -0300

    Exercicio 5

--- Trabalho WEB/Exercicio 4/script.js
+++ Trabalho WEB/Exercicio 4/script.js
@@ -10,12 +10,14 @@ class Lista {
     }
 }
 var lists = []
+ // set this to your table
+
 document.getElementById("myButton").onclick = function(e){
 
     var descricao = document.getElementById('descricao').value;
     var qauntity = document.getElementById('quantity').value;
     var un = document.getElementById('UN').value;
-   
+   var orderArray = 1;
     e.preventDefault();
 
     console.log(descricao)
@@ -24,10 +26,27 @@ document.getElementById("myButton").onclick = function(e){
     if(descricao != " " && qauntity != 0){
         let Mylist = new Lista(descricao,qauntity, un);
         lists.push(Mylist)
-        console.log(lists);
-        console.log(lists.length);
+        console.table(lists);
+
+       
+        var inferno = document.getElementById('resultado')
+        var porra  = display();
+
+        porra.outerHTML
+        inferno.innerHTML = porra
     }
+}
 
-
-    //tela.innerHTML = result;
+function display() {
+    var table = document.createElement("table");
+    for (var i=0; i<lists.length; i++) {
+        var row = table.insertRow();
+        for (var j=0; j<lists[i].length; j++) {
+            var cell = row.insertCell();
+            cell.appendChild(document.createTextNode(lists[i][j]));
+        }
+    }
+    table = String(table)
+    console.log(table)
+    return table;
 }
