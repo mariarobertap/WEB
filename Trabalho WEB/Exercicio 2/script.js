f205e852b0533b973b2d6013f56e207a10e07143
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-27 22:40:46 -0300

    Exercicio 2 resolvido

--- Trabalho WEB/Exercicio 2/script.js
+++ Trabalho WEB/Exercicio 2/script.js
@@ -1,29 +1,28 @@
 
-
 function somar(e, operacao) {
     e.preventDefault();
     var campo = document.getElementById('quantity').value;
     var campo2 = document.getElementById('quantity2').value;
+    var tela = document.getElementById('resultado');
+    campo = Number(campo);
+    campo2 = Number(campo2);
+
+
     if(operacao == '+'){
-        console.log(campo.value);
-        var tela = document.getElementById('resultado');
-        campo = Number(campo);
-        campo2 = Number(campo2);
         campo = campo + campo2
         campo = String(campo)
-        console.log(typeof campo)
         tela.innerHTML = campo 
     }else if(operacao == '-'){
-        console.log(campo.value);
-        var tela = document.getElementById('resultado');
-        tela.innerHTML = 'menos' 
+        campo = campo - campo2
+        campo = String(campo)
+        tela.innerHTML = campo 
     }else if(operacao == '/'){
-        console.log(campo.value);
-        var tela = document.getElementById('resultado');
-        tela.innerHTML = 'asfas' 
+        campo = campo / campo2
+        campo = String(campo)
+        tela.innerHTML = campo 
     }else if(operacao == '*'){
-        console.log(campo.value);
-        var tela = document.getElementById('resultado');
-        tela.innerHTML = 'asdas' 
+        campo = campo * campo2
+        campo = String(campo)
+        tela.innerHTML = campo 
     }
 }
