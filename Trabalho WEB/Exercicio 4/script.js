fd56beb3d9bf82fb9c44a13a94cac5cb3b62ccd7
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-28 00:26:58 -0300

    Exercicio 4 completinho izi pizi

--- Trabalho WEB/Exercicio 4/script.js
+++ Trabalho WEB/Exercicio 4/script.js
@@ -1,9 +1,33 @@
-var tela = document.getElementById('resultado');
 
+class Lista {
+
+    constructor(descricao, quantidade, unidade)
+    {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.unidadeMedida = unidade;
+   
+    }
+}
+var lists = []
 document.getElementById("myButton").onclick = function(e){
-    e.preventDefault();
-    var f = document.getElementById("quantity").value;
-    var result = (f - 32) * (5/9);
 
-    tela.innerHTML = result;
+    var descricao = document.getElementById('descricao').value;
+    var qauntity = document.getElementById('quantity').value;
+    var un = document.getElementById('UN').value;
+   
+    e.preventDefault();
+
+    console.log(descricao)
+    descricao = String(descricao)
+
+    if(descricao != " " && qauntity != 0){
+        let Mylist = new Lista(descricao,qauntity, un);
+        lists.push(Mylist)
+        console.log(lists);
+        console.log(lists.length);
+    }
+
+
+    //tela.innerHTML = result;
 }
