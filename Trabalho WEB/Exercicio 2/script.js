7c9eb8f8e3eb6ffbb0b14a382339d604b4f27dd0
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-29 19:58:45 -0300

    Exercicio 2 refatorado

--- Trabalho WEB/Exercicio 2/script.js
+++ Trabalho WEB/Exercicio 2/script.js
@@ -4,25 +4,32 @@ function somar(e, operacao) {
     var campo = document.getElementById('quantity').value;
     var campo2 = document.getElementById('quantity2').value;
     var tela = document.getElementById('resultado');
+    var resultado;
     campo = Number(campo);
     campo2 = Number(campo2);
 
+    switch (operacao){
+        case '+':
+            campo = campo + campo2
+            campo = String(campo)
+        break;
+        case '-':
+            campo = campo - campo2
+            campo = String(campo)
 
-    if(operacao == '+'){
-        campo = campo + campo2
-        campo = String(campo)
-        tela.innerHTML = campo 
-    }else if(operacao == '-'){
-        campo = campo - campo2
-        campo = String(campo)
-        tela.innerHTML = campo 
-    }else if(operacao == '/'){
-        campo = campo / campo2
-        campo = String(campo)
-        tela.innerHTML = campo 
-    }else if(operacao == '*'){
-        campo = campo * campo2
-        campo = String(campo)
-        tela.innerHTML = campo 
+        break;
+        case '/':
+            campo = campo / campo2
+            campo = String(campo)
+
+        break;
+        case '*':
+            campo = campo * campo2
+            campo = String(campo)
+        break;
+
     }
+
+    tela.innerHTML = "Resultado = " + campo  
+
 }
