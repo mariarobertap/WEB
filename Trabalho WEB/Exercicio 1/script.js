40986d084b0719e8d0fb8ba06498a114052272c3
Maria Roberta <mariarobertaprado@outlook.com>
2021-08-29 18:46:40 -0300

    Exercicio 1 refatorado

--- Trabalho WEB/Exercicio 1/script.js
+++ Trabalho WEB/Exercicio 1/script.js
@@ -2,18 +2,19 @@
 /*              Exercicio 2
 Alterar a cor de fundo da div abaixo para #fdd
 */
-var parent = document.getElementById('resolution1');
-var child_nodes = parent.childNodes;
-console.log("child_nodes.length: " + child_nodes.length);
-console.log(child_nodes[1]);
-var teste = child_nodes[1];
-teste.style.backgroundColor = '#fdd';
+var exercicio1Pai = document.getElementById('resolution1');
+var child_nodes = exercicio1Pai.childNodes;
+var square1 = child_nodes[1];
+square1.style.backgroundColor = '#fdd';
 
 /*              Exercicio 3
 Adicionar um parágrafo com um texto e altere o alinhamento para que o texto fique a esquerda
 */
-//var element = document.getElementById('resolution2');
-//element.innerHTML = '<p align="top"> Meu titulo! </p>'
+
+var parent2 = document.getElementById('resolution2');
+var child_nodes2 = parent2.childNodes;
+var square2 = child_nodes2[1];
+square2.innerHTML = '<p align="left"> Meu titulo! </p>'
 
 
 /*              Exercicio 4
@@ -22,14 +23,22 @@ Ao passar o mouse sobre os quadrados, defina uma borda de 2px
 
 var p = document.getElementById("resolution1");//
 
-teste.onmouseover = function(e){
-    teste.style.border = '2px solid black';
+square1.onmouseover  = function(e){
+    square1.style.border = '2px solid black';
 }
 
-teste.onmouseleave = function(e){
-    teste.style.border = '2px '; 
+square1.onmouseleave = function(e){
+    square1.style.border = '2px '; 
 }
 
+
+square2.onmouseover  = function(e){
+    square2.style.border = '2px solid black';
+}
+
+square2.onmouseleave = function(e){
+    square2.style.border = '2px '; 
+}
 /*              Exercicio 5
 Criar um botão utilizando a classe .btn que ao ser clicado esconda todos os quadrados
 */
@@ -38,15 +47,17 @@ var butao = document.getElementById('resolution5');
 butao.innerHTML = '<button class ="btn" onclick="hideSquares()"> Click me if you want to HIDE all squares! </button>'
 
 function hideSquares() {
-    document.getElementById("resolution1").style.display = "none";  
-    document.getElementById("resolution2").style.display = "none";  
+ 
+    square1.style.display = "none"
+    square2.style.display = "none"
     butao.innerHTML = '<button class ="btn" onclick="showSquares()"> Click me if you want to SHOW all squares! </button>'
 
 }
 
 function showSquares() {
-    document.getElementById("resolution1").style.display = "";  
-    document.getElementById("resolution2").style.display = "";  
+  
+    square1.style.display = ""
+    square2.style.display = ""
     butao.innerHTML = '<button class ="btn" onclick="hideSquares()"> Click me if you want to HIDE all squares! </button>'
 
 }
